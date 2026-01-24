79e212e3d9df7760de47c23aeaeb34e51fe3d476
Xavicasa96 <xa.casal@uea.edu.ec>
2026-01-24 09:33:07 -0500

    semana7

--- SEMANA2/ejercicio/obj/Debug/net8.0/ejercicio.AssemblyInfo.cs
+++ SEMANA2/ejercicio/obj/Debug/net8.0/ejercicio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ejercicio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c18759d387fb1510ab8ba7dee5cbfbac2ed925d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb11b50ff63485931e693786e5b6904cb37c5675")]
 [assembly: System.Reflection.AssemblyProductAttribute("ejercicio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ejercicio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
